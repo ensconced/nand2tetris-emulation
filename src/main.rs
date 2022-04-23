655f582de0c9a8d385ac7be30597b04b9168c183
Joe Barnett <joebarnett876@gmail.com>
2022-04-23 17:17:43 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod computer;
+mod programmer;
 
 use computer::computer::{bit, Computer};
 use minifb::{Window, WindowOptions};
+use programmer::programmer::get_rom;
 use raqote::{DrawOptions, DrawTarget, PathBuilder, SolidSource, Source};
-use std::fs;
 use std::time::SystemTime;
 
 const WIDTH: usize = 1000;
@@ -57,26 +58,7 @@ fn main() {
     let size = window.get_size();
     let mut dt = DrawTarget::new(size.0 as i32, size.1 as i32);
 
-    let program = fs::read_to_string("./programs/blinky").unwrap();
-    let mut clean_lines: Vec<String> = program
-        .lines()
-        .map(|line| {
-            let clean_line: String = line
-                .chars()
-                .take_while(|ch| ch.is_ascii_digit() || ch.is_whitespace())
-                .collect();
-            clean_line
-        })
-        .filter(|line| line.len() > 0)
-        .collect();
-
-    let mut rom: [i16; 32768] = [0; 32768];
-    for (idx, line) in clean_lines.iter_mut().enumerate() {
-        line.retain(|ch| !ch.is_whitespace());
-        let instruction = u16::from_str_radix(line, 2).unwrap() as i16;
-        rom[idx] = instruction;
-    }
-    let mut computer = Computer::new(rom);
+    let mut computer = Computer::new(get_rom("./programs/blinky"));
 
     let mut last_draw_time = SystemTime::now();
     loop {

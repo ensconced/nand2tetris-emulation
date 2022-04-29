9796da0ade78255071f00a7b621747c9dc6d7564
Joe Barnett <joebarnett876@gmail.com>
2022-04-29 21:16:49 +0100

    more parsing

---
+++ src/assembler/codegen.rs
@@ -0,0 +1,5 @@
+use first_pass::FirstPassResult;
+
+use super::first_pass;
+
+fn codegen(first_pass_result: FirstPassResult) -> String {}
---
+++ src/assembler/first_pass.rs
@@ -0,0 +1,69 @@
+use std::collections::HashMap;
+
+use super::parser::Command;
+
+struct FirstPassResult {
+    resolved_symbols: HashMap<String, usize>,
+    commands_without_labels: Vec<Command>,
+}
+
+fn first_pass(mut commands: impl Iterator<Item = Command>) -> FirstPassResult {
+    let mut resolved_symbols = HashMap::new();
+    let mut commands_without_labels = Vec::new();
+    let mut index = 0;
+    while let Some(command) = commands.next() {
+        if let Command::LCommand { identifier } = command {
+            resolved_symbols.insert(identifier, index);
+        } else {
+            index = index + 1;
+            commands_without_labels.push(command);
+        }
+    }
+    FirstPassResult {
+        resolved_symbols,
+        commands_without_labels,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::assembler::parser::{parse_lines, AValue};
+
+    #[test]
+    fn test_first_pass() {
+        let commands = parse_lines(
+            vec![
+                "(foo)", "A=A+1", "M=M|A", "(bar)", "@foo", "@bar", "(baz)", "@1234",
+            ]
+            .into_iter(),
+        );
+        let FirstPassResult {
+            resolved_symbols,
+            commands_without_labels,
+        } = first_pass(commands);
+        let expected_resolved_symbols = HashMap::from([
+            ("foo".to_string(), 0 as usize),
+            ("bar".to_string(), 2 as usize),
+            ("baz".to_string(), 4 as usize),
+        ]);
+        assert_eq!(resolved_symbols, expected_resolved_symbols);
+
+        let expected_commands_without_labels = vec![
+            Command::CCommand {
+                expr: "A+1".to_string(),
+                dest: Some("A".to_string()),
+                jump: None,
+            },
+            Command::CCommand {
+                expr: "M|A".to_string(),
+                dest: Some("M".to_string()),
+                jump: None,
+            },
+            Command::ACommand(AValue::Symbolic("foo".to_string())),
+            Command::ACommand(AValue::Symbolic("bar".to_string())),
+            Command::ACommand(AValue::Numeric("1234".to_string())),
+        ];
+        assert_eq!(commands_without_labels, expected_commands_without_labels);
+    }
+}

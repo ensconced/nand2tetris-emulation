1d9cb24b076d9e1c8e1dcd74850b548aee37a95e
Joe Barnett <joe.barnett@elliptic.co>
2022-05-06 08:43:51 +0100

    lexer seems to be working but it needs more tests

--- src/assembler/lexer.rs
+++ src/assembler/lexer.rs
@@ -1,18 +1,28 @@
 use lazy_static::lazy_static;
 use regex::Regex;
+use std::iter;
 
-enum Token {
+#[derive(Debug, PartialEq)]
+struct Token {
+    length: usize,
+    kind: TokenKind,
+}
+
+#[derive(Debug, PartialEq)]
+enum TokenKind {
+    Comment,
+    Whitespace,
     Identifier(String),
     Number(String),
+    Operator(String),
     At,
     LParen,
     RParen,
     Equals,
-    Operator,
     Semicolon,
 }
 
-fn lex(line: String) -> Vec<Token> {
+fn get_first_token(string: &str) -> Option<Token> {
     lazy_static! {
         static ref COMMENT: Regex = Regex::new(r"^//.*").expect("failed to compile COMMENT regex");
         static ref WHITESPACE: Regex =
@@ -20,32 +30,108 @@ fn lex(line: String) -> Vec<Token> {
         static ref IDENTIFIER: Regex = Regex::new(r"^[a-zA-Z:$_.][0-9a-zA-Z:$_.]*")
             .expect("failed to compile IDENTIFIER regex");
         static ref NUMBER: Regex = Regex::new(r"^[0-9]+").expect("failed to compile NUMBER regex");
+        static ref AT: Regex = Regex::new(r"^@").expect("failed to compile AT regex");
+        static ref LPAREN: Regex = Regex::new(r"^(").expect("failed to compile LPAREN regex");
+        static ref RPAREN: Regex = Regex::new(r"^)").expect("failed to compile RPAREN regex");
+        static ref EQUALS: Regex = Regex::new(r"^=").expect("failed to compile EQUALS regex");
         static ref OPERATOR: Regex =
             Regex::new(r"^[+-|&!]+").expect("failed to compile OPERATOR regex");
+        static ref SEMICOLON: Regex = Regex::new(r"^;").expect("failed to compile SEMICOLON regex");
     }
-    let mut result = Vec::new();
-    let line_clone = line.clone();
-    let mut chars = line_clone.chars();
-    if let Some(lexeme) = COMMENT.find(&line) {
-        chars.nth(lexeme.as_str().len() - 1);
-    } else if let Some(lexeme) = WHITESPACE.find(&line) {
-        chars.nth(lexeme.as_str().len() - 1);
-    } else if let Some(lexeme) = IDENTIFIER.find(&line) {
-        chars.nth(lexeme.as_str().len() - 1);
-        result.push(Token::Identifier(lexeme.as_str().to_string()));
+    if string.is_empty() {
+        None
+    } else if let Some(match_result) = COMMENT.find(string) {
+        let match_string = match_result.as_str();
+        Some(Token {
+            length: match_string.len(),
+            kind: TokenKind::Comment,
+        })
+    } else if let Some(match_result) = WHITESPACE.find(string) {
+        let match_string = match_result.as_str();
+        Some(Token {
+            length: match_string.len(),
+            kind: TokenKind::Whitespace,
+        })
+    } else if let Some(match_result) = IDENTIFIER.find(string) {
+        let match_string = match_result.as_str();
+        Some(Token {
+            length: match_string.len(),
+            kind: TokenKind::Identifier(match_string.to_string()),
+        })
+    } else if let Some(match_result) = NUMBER.find(string) {
+        let match_string = match_result.as_str();
+        Some(Token {
+            length: match_string.len(),
+            kind: TokenKind::Number(match_string.to_string()),
+        })
+    } else if let Some(match_result) = OPERATOR.find(string) {
+        let match_string = match_result.as_str();
+        Some(Token {
+            length: match_string.len(),
+            kind: TokenKind::Operator(match_string.to_string()),
+        })
+    } else if let Some(match_result) = AT.find(string) {
+        let match_string = match_result.as_str();
+        Some(Token {
+            length: match_string.len(),
+            kind: TokenKind::At,
+        })
+    } else if let Some(match_result) = LPAREN.find(string) {
+        let match_string = match_result.as_str();
+        Some(Token {
+            length: match_string.len(),
+            kind: TokenKind::LParen,
+        })
+    } else if let Some(match_result) = RPAREN.find(string) {
+        let match_string = match_result.as_str();
+        Some(Token {
+            length: match_string.len(),
+            kind: TokenKind::RParen,
+        })
+    } else if let Some(match_result) = EQUALS.find(string) {
+        let match_string = match_result.as_str();
+        Some(Token {
+            length: match_string.len(),
+            kind: TokenKind::Equals,
+        })
+    } else if let Some(match_result) = SEMICOLON.find(string) {
+        let match_string = match_result.as_str();
+        Some(Token {
+            length: match_string.len(),
+            kind: TokenKind::Semicolon,
+        })
+    } else {
+        panic!("failed to lex token at \"{}\"", string);
     }
-    // TODO etc...
-    result
+}
+
+fn get_tokens(string: String) -> Box<dyn Iterator<Item = Token>> {
+    if let Some(first_token) = get_first_token(&string) {
+        let len = first_token.length;
+        Box::new(iter::once(first_token).chain(get_tokens(string.chars().skip(len).collect())))
+    } else {
+        Box::new(iter::empty())
+    }
 }
 
 #[test]
-fn test_foo() {
+fn test_get_token() {
+    let tokens: Vec<Token> = get_tokens("HEY THERE".to_string()).collect();
     assert_eq!(
-        COMMENT.find("// hello there").unwrap().as_str(),
-        "// hello there"
-    );
-    assert_eq!(WHITESPACE.find("   ").unwrap().as_str(), "   ");
-    assert_eq!(IDENTIFIER.find("hey").unwrap().as_str(), "hey");
-    assert_eq!(NUMBER.find("1234").unwrap().as_str(), "1234");
-    assert_eq!(OPERATOR.find("+").unwrap().as_str(), "+");
+        tokens,
+        vec![
+            Token {
+                length: 3,
+                kind: TokenKind::Identifier("HEY".to_string())
+            },
+            Token {
+                length: 1,
+                kind: TokenKind::Whitespace,
+            },
+            Token {
+                length: 5,
+                kind: TokenKind::Identifier("THERE".to_string()),
+            },
+        ]
+    )
 }

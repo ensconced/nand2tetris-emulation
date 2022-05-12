a4d6c3e384ecbb319a3a65e9f0bf54108239f788
Joe Barnett <joebarnett876@gmail.com>
2022-05-12 20:40:01 +0100

    wip

--- src/compilers/parser_utils.rs
+++ src/compilers/parser_utils.rs
@@ -1,4 +1,4 @@
-use super::tokenizer::Token;
+use super::tokenizer::{Token, TokenDef, Tokenizer};
 use std::iter::Peekable;
 
 pub fn maybe_take<TokenKind>(
@@ -14,4 +14,25 @@ where
         }
     }
     None
+}
+
+type PeekableTokens<TokenKind> = Peekable<Box<dyn Iterator<Item = Token<TokenKind>>>>;
+type LineParser<ParsedLine, TokenKind> =
+    fn(tokens: PeekableTokens<TokenKind>, line_number: usize) -> Option<ParsedLine>;
+type TokenDefs<TokenKind> = Vec<TokenDef<TokenKind>>;
+
+pub fn parse_by_line<'a, ParsedLine, TokenKind>(
+    source: &'a str,
+    line_parser: LineParser<ParsedLine, TokenKind>,
+    token_defs: TokenDefs<TokenKind>,
+) -> impl Iterator<Item = ParsedLine> + 'a
+where
+    ParsedLine: 'static,
+{
+    let lines = source.lines().map(|line| line.to_string());
+    let tokenizer = Tokenizer::new(token_defs);
+    lines.enumerate().filter_map(move |(line_idx, line)| {
+        let tokens = tokenizer.tokenize(&line).peekable();
+        line_parser(tokens, line_idx + 1)
+    })
 }

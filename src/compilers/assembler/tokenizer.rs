79007592856e2506f785e0ef2ecc119238ed3411
Joe Barnett <joebarnett876@gmail.com>
2022-05-14 17:22:31 +0100

    wip

--- src/compilers/assembler/tokenizer.rs
+++ src/compilers/assembler/tokenizer.rs
@@ -14,21 +14,21 @@ pub enum TokenKind {
     Semicolon,
 }
 
+use TokenKind::*;
+
 pub fn token_defs() -> Vec<TokenDef<TokenKind>> {
     vec![
-        TokenDef::new(r"//.*", |_| TokenKind::Comment),
+        TokenDef::new(r"//.*", |_| Comment),
         TokenDef::new(r"[AMD]{1,3}=", |src| {
-            TokenKind::Destination(src[0..src.len() - 1].to_string())
+            Destination(src[0..src.len() - 1].to_string())
         }),
-        TokenDef::new(r"\s+", |_| TokenKind::Whitespace),
-        TokenDef::new(r"(\||\+|-|&|!)", |src| TokenKind::Operator(src)),
-        TokenDef::new(r"[a-zA-Z:$_.][0-9a-zA-Z:$_.]*", |src| {
-            TokenKind::Identifier(src)
-        }),
-        TokenDef::new(r"[0-9]+", |src| TokenKind::Number(src)),
-        TokenDef::new(r"@", |_| TokenKind::At),
-        TokenDef::new(r"\(", |_| TokenKind::LParen),
-        TokenDef::new(r"\)", |_| TokenKind::RParen),
-        TokenDef::new(r";", |_| TokenKind::Semicolon),
+        TokenDef::new(r"\s+", |_| Whitespace),
+        TokenDef::new(r"(\||\+|-|&|!)", Operator),
+        TokenDef::new(r"[a-zA-Z:$_.][0-9a-zA-Z:$_.]*", Identifier),
+        TokenDef::new(r"[0-9]+", Number),
+        TokenDef::new(r"@", |_| At),
+        TokenDef::new(r"\(", |_| LParen),
+        TokenDef::new(r"\)", |_| RParen),
+        TokenDef::new(r";", |_| Semicolon),
     ]
 }

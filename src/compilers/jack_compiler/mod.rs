b0664095b36fb1e3707f8666863e5d3b187e1a0b
Joe Barnett <joebarnett876@gmail.com>
2022-06-18 06:17:10 +0100

    wip

--- src/compilers/jack_compiler/mod.rs
+++ src/compilers/jack_compiler/mod.rs
@@ -12,8 +12,8 @@ mod codegen;
 mod parser;
 mod tokenizer;
 
-fn compile_source_module(source_module: &SourceModule) -> String {
-    let class = parse(&source_module.source);
+fn compile_source(source: &str) -> String {
+    let class = parse(source);
     let mut code_generator = CodeGenerator::new();
     code_generator.vm_code(class)
 }
@@ -21,7 +21,7 @@ fn compile_source_module(source_module: &SourceModule) -> String {
 fn compile(src_path: &Path, dest_path: &Path) {
     let source_modules = get_source_modules(src_path).expect("failed to get source modules");
     for source_module in source_modules {
-        let vm_code = compile_source_module(&source_module);
+        let vm_code = compile_source(&source_module.source);
         let mut module_dest_path = if source_module.entrypoint_is_dir {
             dest_path.join(source_module.filename)
         } else {
@@ -29,5 +29,23 @@ fn compile(src_path: &Path, dest_path: &Path) {
         };
         module_dest_path.set_extension("vm");
         fs::write(module_dest_path, vm_code).expect("failed to write compilation output")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::compile_source;
+
+    #[test]
+    fn test_foo() {
+        let source = "
+            class foo {
+                method void bar () {
+                    var int a;
+                    let a = 1 + 2 + 3;
+                }
+            }
+        ";
+        assert_eq!(compile_source(source), "asdf");
     }
 }

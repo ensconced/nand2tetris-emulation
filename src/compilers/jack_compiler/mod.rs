23b783e042c7e034fc7e5b4aae818cf1f06cdf27
Joe Barnett <joebarnett876@gmail.com>
2022-07-13 18:13:56 +0100

    wip

--- src/compilers/jack_compiler/mod.rs
+++ src/compilers/jack_compiler/mod.rs
@@ -553,7 +553,7 @@ mod tests {
                     let nested_arr = Memory.alloc(array_count);
                     let j = 0;
                     // while (j < array_count) {
-                    while (j < 60) {
+                    while (j < 28) {
                         let arr = Memory.alloc(array_length);
                         let k = 0;
                         // TODO - this causes an error - reinstate it and find out what's going on!
@@ -575,8 +575,8 @@ mod tests {
                 (8, vec![]),
                 (16, vec![]),
                 (32, vec![]),
-                (64, vec![2240]),
-                (128, vec![]),
+                (64, vec![]),
+                (128, vec![2176]),
                 (256, vec![]),
                 (512, vec![2560]),
                 (1024, vec![3072]),

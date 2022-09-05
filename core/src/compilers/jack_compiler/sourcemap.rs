ad19287110b008ef3b2d7b65eb2d0903e120fd26
Joe Barnett <joebarnett876@gmail.com>
2022-09-05 22:22:13 +0100

    wip

--- core/src/compilers/jack_compiler/sourcemap.rs
+++ core/src/compilers/jack_compiler/sourcemap.rs
@@ -1,5 +1,5 @@
 use serde::Serialize;
-use std::{collections::HashMap, ops::Range};
+use std::{collections::HashMap, ops::Range, path::Path};
 use ts_rs::TS;
 
 #[derive(Serialize, TS)]
@@ -15,7 +15,7 @@ pub struct NodeInfo {
 #[ts(export)]
 #[ts(export_to = "../bindings/")]
 pub struct SourceMap {
-    pub token_idx_to_jack_node_idxs: HashMap<usize, Vec<usize>>,
+    pub token_idx_to_jack_node_idxs: HashMap<&'static Path, HashMap<usize, Vec<usize>>>,
     pub jack_nodes: Vec<NodeInfo>,
     pub jack_node_idx_to_vm_command_idx: HashMap<usize, Vec<usize>>,
     pub vm_command_idx_to_jack_node_idx: HashMap<usize, usize>,
@@ -31,15 +31,17 @@ impl SourceMap {
         }
     }
 
-    pub fn record_jack_node(&mut self, token_range: Range<usize>, child_node_idxs: Vec<usize>) -> usize {
+    pub fn record_jack_node(&mut self, filename: &'static Path, token_range: Range<usize>, child_node_idxs: Vec<usize>) -> usize {
         let node_idx = self.jack_nodes.len();
         self.jack_nodes.push(NodeInfo {
             token_range: token_range.clone(),
             child_node_idxs,
             index: node_idx,
         });
+
+        let token_lookup = self.token_idx_to_jack_node_idxs.entry(filename).or_default();
         for token_idx in token_range {
-            let token_jack_node_idxs = self.token_idx_to_jack_node_idxs.entry(token_idx).or_default();
+            let token_jack_node_idxs = token_lookup.entry(token_idx).or_default();
             token_jack_node_idxs.push(node_idx);
         }
         node_idx

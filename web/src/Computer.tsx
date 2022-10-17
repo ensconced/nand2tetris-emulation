86361d1982bb3dc299579e2f2e8017d4a64ddb32
Joe Barnett <joebarnett876@gmail.com>
2022-10-17 09:19:01 +0100

    nothing much

--- web/src/Computer.tsx
+++ web/src/Computer.tsx
@@ -16,11 +16,9 @@ const wordDisplayBaseOptions = ["binary", "decimal"];
 
 export default function Computer() {
   const [wordDisplayBaseIdx, setWordDisplayBaseIdx] = useState(0);
-  const wordDisplayBase = wordDisplayBaseOptions[wordDisplayBaseIdx];
-
   const ram = useMemo(() => {
     const ramString = get_formatted_ram(computer.ram, wordDisplayBaseIdx);
-    return ramString.split("\n");
+    return ramString.split(/(?=\n)/);
   }, [wordDisplayBaseIdx]);
 
   return (
@@ -33,7 +31,7 @@ export default function Computer() {
       />
       <code style={{ textAlign: "right" }} className="code-panel">
         {ram.map((line) => (
-          <span>{`${line}\n`}</span>
+          <span>{line}</span>
         ))}
       </code>
     </div>

41bdb1644c9b73f271c5fa073ddb2815d44dd943
Joe Barnett <joebarnett876@gmail.com>
2024-02-04 17:06:03 +0000

    include nodejs in docker

---
+++ fpga/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -eux pipefile
+
+cd fpga
+
+node ./verilog/romgen.js blinky >counter_test/components/rom.mem
+
+ICARUS_OUT_FILENAME="icarus.out"
+ALL_VERILOG_FILES=$(find components -name '*.v')
+iverilog -v -gassertions -g2012 -o "$ICARUS_OUT_FILENAME" $ALL_VERILOG_FILES
+vvp "$ICARUS_OUT_FILENAME" -v -vcd
+echo "tests passed!"
+
+make -C counter_test
+make download -C counter_test

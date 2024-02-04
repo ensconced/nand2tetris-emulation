ecca8791f2b00e2fe190e1780f8fd6c32df24151
Joe Barnett <joebarnett876@gmail.com>
2024-02-04 17:49:16 +0000

    include iverilog in docker

--- fpga/build.sh
+++ fpga/build.sh
@@ -7,7 +7,7 @@ cd fpga
 node ./verilog/romgen.js blinky >counter_test/components/rom.mem
 
 ICARUS_OUT_FILENAME="icarus.out"
-ALL_VERILOG_FILES=$(find components -name '*.v')
+ALL_VERILOG_FILES=$(find counter_test/components -name '*.v')
 iverilog -v -gassertions -g2012 -o "$ICARUS_OUT_FILENAME" $ALL_VERILOG_FILES
 vvp "$ICARUS_OUT_FILENAME" -v -vcd
 echo "tests passed!"

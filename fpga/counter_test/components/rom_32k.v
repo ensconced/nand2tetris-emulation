0a7d933a0ea8f81d4ecde50954dfb74c6dbcaec5
Joe Barnett <joebarnett876@gmail.com>
2024-02-04 22:07:29 +0000

    fix

--- fpga/counter_test/components/rom_32k.v
+++ fpga/counter_test/components/rom_32k.v
@@ -1,7 +1,7 @@
 module rom_32k(input [14:0] addr, output [15:0] out);
     reg [15:0] memory [0:32767];
     initial begin
-        $readmemb("/f4pga-examples/xc7/computer/counter_test/rom.mem", memory);
+        $readmemb("/f4pga-examples/xc7/counter_test/components/rom.mem", memory);
     end
     assign out = memory[addr];
 endmodule

3ce1d4ad3292311f847166d1950bfb357cea4bd2
Joe Barnett <joebarnett876@gmail.com>
2024-02-04 11:33:46 +0000

    working blinky

--- fpga/counter_test/components/rom_32k.v
+++ fpga/counter_test/components/rom_32k.v
@@ -1,7 +1,7 @@
 module rom_32k(input [14:0] addr, output [15:0] out);
     reg [15:0] memory [0:32767];
     initial begin
-        $readmemb(`ROM_FILE, memory);
+        $readmemb("/f4pga-examples/xc7/computer/components/rom.mem", memory);
     end
     assign out = memory[addr];
 endmodule
